3b1f6d679d3a10621bec1bfda3cddb4266357fe4
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-19 21:34:30 -0400

    use colors for server running and db connected console logs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 // Custom logger (replaced by morgan)
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -29,11 +30,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server and exit process
   server.close(() => process.exit(1));
 })
