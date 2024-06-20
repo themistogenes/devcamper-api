42d4aaf1350318f76257ca5116d029ba03426a3f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-20 14:37:37 -0400

    get all bootcamps GET request

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,11 +3,18 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all bootcamps'
-  })
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find({});
+
+    res.status(200).json({
+      success: true,
+      msg: 'Retrieved all bootcamps',
+      data: bootcamps
+    })
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Get single bootcamp
