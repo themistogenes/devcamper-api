7f376233a60e76d07bb33ad4810fb0624d7259ff
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-07-10 19:58:46 -0400

    get single course

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-  getCourses
+  getCourses,
+  getCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
@@ -8,5 +9,8 @@ const router = express.Router({ mergeParams: true });
 // /api/v1/courses
 router.route('/')
   .get(getCourses);
+
+router.route('/:id')
+  .get(getCourse);
 
 module.exports = router;
