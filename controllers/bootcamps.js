a596c7a3098abee79fd7e488dd895f7195341255
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-01 15:39:42 -0400

    add User to Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,81 +8,96 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    res.status(200).json(res.advancedResults);
+  res.status(200).json(res.advancedResults);
 })
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-    // Prevent a properly formatted id from sending back a 200 status
-    if (!bootcamp) {
-      // Must return to avoid sending back multiple headers
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
+  // Prevent a properly formatted id from sending back a 200 status
+  if (!bootcamp) {
+    // Must return to avoid sending back multiple headers
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      msg: `Retrieved bootcamp ${req.params.id}`,
-      data: bootcamp
-    })
+  res.status(200).json({
+    success: true,
+    msg: `Retrieved bootcamp ${req.params.id}`,
+    data: bootcamp
+  })
 })
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.create(req.body);
+  // Add user to req.body
+  req.body.user = req.user.id;
 
-    res.status(201).json({
-      success: true,
-      msg: 'Created new bootcamp',
-      data: bootcamp
-    })
+  // Check if any bootcamp is already published by user
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`, 400
+      )
+    )
+  }
+
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    msg: 'Created new bootcamp',
+    data: bootcamp
+  })
 })
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(
-      req.params.id, 
-      req.body, 
-      {
-        new: true,
-        runValidators: true
-      });
+  const bootcamp = await Bootcamp.findByIdAndUpdate(
+    req.params.id, 
+    req.body, 
+    {
+      new: true,
+      runValidators: true
+    });
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
-  
-    res.status(200).json({
-      success: true,
-      msg: `Updated bootcamp ${req.params.id}`,
-      data: bootcamp
-    })    
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    msg: `Updated bootcamp ${req.params.id}`,
+    data: bootcamp
+  })    
 })
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+  }
 
-    await bootcamp.deleteOne();
+  await bootcamp.deleteOne();
 
-    res.status(200).json({
-      success: true,
-      msg: `Deleted bootcamp ${req.params.id}`,
-      data: {}
-    })
+  res.status(200).json({
+    success: true,
+    msg: `Deleted bootcamp ${req.params.id}`,
+    data: {}
+  })
 })
 
 // @desc    Get bootcamps within a radius
