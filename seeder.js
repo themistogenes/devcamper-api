2d0f9d85fa7f77a3d368d965dc88fd7bc5c40109
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-01 15:34:17 -0400

    add users to seeder

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -24,11 +25,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 )
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+)
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data imported into DB...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -41,6 +47,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data deleted from DB...'.red.inverse);
     process.exit();
   } catch (err) {
