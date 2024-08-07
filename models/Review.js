4dbff07e29471e0dd3f4323bb4017f6be84d38a4
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-07 19:03:00 -0400

    calculate average rating of reviews for a bootcamp

--- models/Review.js
+++ models/Review.js
@@ -36,4 +36,42 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting multiple reviews per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+// Static method to get average rating of reviews for bootcamp
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+  console.log('Calculating average rating of reviews for bootcamp...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(
+      bootcampId, 
+      {
+        averageRating: obj[0].averageRating
+      }
+    )
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function() {
+  this.constructor.getAverageRating(this.bootcamp);
+})
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function() {
+  this.constructor.getAverageRating(this.bootcamp);
+})
+
 module.exports = mongoose.model('Review', ReviewSchema);
