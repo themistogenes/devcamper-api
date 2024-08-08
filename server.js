f1d3ff9bb13cae32abaf886c4e8ba1a635920441
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-08 14:03:18 -0400

    enable CORS

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -65,6 +66,9 @@ app.use(limiter);
 
 // Prevent http param pollution
 app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
