ba81d991007b17aba9e64bc146ec8b7ce6f9a6bc
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-08 13:42:48 -0400

    prevent cross site scripting (xss) attacks with xss-clean

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -49,6 +50,9 @@ app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
